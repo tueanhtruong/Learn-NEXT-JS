262bc331efb171573c5d86695ae35a953441f9c5
Tue Truong <tue_truong@datahouse.com>
2022-05-01 22:16:56 +0700

    fix: build

--- pages/fetching/helpers.ts
+++
@@ -1,7 +0,0 @@
-const WAITING_TIME = 200;
-
-export const wrapIntoResponse = (data: any) => {
-  return new Promise((resolve) => {
-    setTimeout(() => resolve({ ok: true, data }), WAITING_TIME);
-  });
-};
