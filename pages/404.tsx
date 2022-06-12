f6eb9cefb1a8882fbb03ba63f429e4105305fa73
Tue Truong <tue_truong@datahouse.com>
2022-06-13 00:54:48 +0700

    fix: build

--- pages/404.tsx
+++ pages/404.tsx
@@ -2,8 +2,6 @@ import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
 import Link from 'next/link';
-import { PATHS } from '../app-config/constants';
-// import styles from '../styles/Home.module.scss';
 
 const Home: NextPage = () => {
   return (
